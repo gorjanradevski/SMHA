25e25d853cd88c084145c551a8309bfd354115de
gorjanradevski <gorjanradevski@gmail.com>
2019-04-23 02:35:25 +0200

    output hidden size is *2 because of the rnn

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -74,9 +74,9 @@ def train(
     logger.info("Validation dataset created...")
 
     evaluator_train = Evaluator()
-    # The number of features at the output will be: rnn_hidden_size * attn_size2
+    # The number of features at the output will be: rnn_hidden_size * attn_size2 * 2
     evaluator_val = Evaluator(
-        len(val_image_paths), hparams.rnn_hidden_size * hparams.attn_size2
+        len(val_image_paths), hparams.rnn_hidden_size * hparams.attn_size2 * 2
     )
 
     logger.info("Evaluators created...")
